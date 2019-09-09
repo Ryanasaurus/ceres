5277c04e45290c4f7676f024d7a4e80fc1245dd7
Ryanasaurus <ryanasaurus97@gmail.com>
2019-09-09 16:32:22 +1200

    added new player scripts, and player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed, 0.0f);
+
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
