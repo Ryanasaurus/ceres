53c552dfd52314f2af655ae8d4cf81a945000ea9
Ryanasaurus <ryanasaurus97@gmail.com>
2019-09-25 20:28:05 +1200

    added rotation to player, now follows mouse cursor (movement still relative to the screen, not player direction)

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     public float moveSpeed = 5;
+    private Vector3 targetPosition;
+    private Vector2 rotateVector;
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -15,5 +17,12 @@ public class PlayerMovementController : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed, 0.0f);
 
         transform.position = transform.position + movement * Time.deltaTime;
+
+        //rotate player to cursor
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rotateVector = targetPosition - transform.position;
+        float rotateAngle = (Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg) - 90;
+        Quaternion rotation = Quaternion.AngleAxis(rotateAngle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 5 * Time.deltaTime);
     }
 }
