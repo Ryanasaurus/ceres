fde0e224c8dd1d42e1dd0fb0c47577c9d4ae2cba
Ryanasaurus <ryanasaurus97@gmail.com>
2019-10-22 19:56:03 +1300

    updated space demo scene

--- Assets/Scripts/ShipMovementController.cs
+++ Assets/Scripts/ShipMovementController.cs
@@ -6,23 +6,36 @@ public class ShipMovementController : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float turnSpeed = 1f;
+
     private Vector3 targetPosition;
     private Vector2 rotateVector;
+    private Rigidbody2D rb2D;
 
     void Start()
     {    
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
-    void FixedUpdate()
+    void Update()
     {
         if(Input.GetMouseButton(0)) {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             rotateVector = targetPosition - transform.position;
 
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
+            Move();
 
-            float rotateAngle = (Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg) - 90;
-            Quaternion rotation = Quaternion.AngleAxis(rotateAngle, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+            Rotate();
         }
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
+    }
+
+    void Rotate()
+    {
+        float rotateAngle = (Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg) - 90;
+        Quaternion rotation = Quaternion.AngleAxis(rotateAngle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
     }
 }
