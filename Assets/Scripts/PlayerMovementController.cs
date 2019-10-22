1e5b335cab670669294b60d66267524d34fb4c53
Ryanasaurus <ryanasaurus97@gmail.com>
2019-10-22 19:37:29 +1300

    updated player movement to work with future collisions, and use physics based movement

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,18 +7,32 @@ public class PlayerMovementController : MonoBehaviour
     public float moveSpeed = 5;
     private Vector3 targetPosition;
     private Vector2 rotateVector;
+
+    private Rigidbody2D rb2D;
     void Start()
     {
-
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed, 0.0f);
-
-        transform.position = transform.position + movement * Time.deltaTime;
+        Move();
 
         //rotate player to cursor
+        RotateToCursor();        
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed, 0.0f);
+
+        // transform.position = transform.position + movement * Time.deltaTime;
+
+        rb2D.velocity = new Vector2(movement.x, movement.y);
+    }
+
+    private void RotateToCursor()
+    {
         targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         rotateVector = targetPosition - transform.position;
         float rotateAngle = (Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg) - 90;
